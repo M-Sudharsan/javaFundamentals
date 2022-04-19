55988ff36c30d8ce9ece0c74214c12a665c702f1
Sudharsan Murali <remomms@gmail.com>
2022-04-19 13:00:04 +0530

    created a simple assignment code

---
+++ javaCourseTAU/src/chapter2/MadLibs.java
@@ -0,0 +1,30 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class MadLibs {
+    public static void main(String[] args) {
+        /*
+         * Create a program that asks the user for a season of the year, then a whole
+         * number, then an adjective. Use the input to complete the sentence below and
+         * output the result:
+         * 
+         * "On a [adjective] [season of the year] day, I drink a minimum of [whole number] cops of coffee."
+         */
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a season of the year :\t");
+        String season = scanner.next();
+
+        System.out.println("Enter a whole number :\t");
+        int wholeNumber = scanner.nextInt();
+
+        System.out.println("Enter an adjective :\t");
+        String adjective = scanner.next();
+        scanner.close();
+
+        System.out.println(
+                "On a " + adjective + " " + season + " day, I drink a minimum of " + wholeNumber + " cups of coffee.");
+    }
+}
