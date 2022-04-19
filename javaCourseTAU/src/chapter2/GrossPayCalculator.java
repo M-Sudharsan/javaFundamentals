51843563decd296e3a3579b50b162390c7e96f20
Sudharsan Murali <remomms@gmail.com>
2022-04-19 12:39:43 +0530

    created new java project and wrote a sample code
    1. A sample stock project was created
    2. Wrote a sample code for a GrossPayCalculator

---
+++ javaCourseTAU/src/chapter2/GrossPayCalculator.java
@@ -0,0 +1,23 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class GrossPayCalculator {
+    public static void main(String[] args) {
+        // 1. Get the number of hours worked
+        System.out.println("Enter the number of hours the employee worked: ");
+        Scanner scanner = new Scanner(System.in);
+        int hours = scanner.nextInt();
+
+        // 2. Get the hourly pay rate
+        System.out.println("Enetr the employee's pay rate: ");
+        double payRate = scanner.nextDouble();
+        scanner.close();
+
+        // 3. Multiply hours and pay rate
+        double grossPay = hours * payRate;
+
+        // 4. Display the Result
+        System.out.println("The employee's gross pay is: $" + grossPay);
+    }
+}
