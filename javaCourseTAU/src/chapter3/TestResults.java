1cb49a2d0ace27e82454afd36aee28593b08551f
sudharsan <sudharsan048@cicgogo.com>
2022-05-18 10:05:28 +0530

    Lesson 3d completed

---
+++ javaCourseTAU/src/chapter3/TestResults.java
@@ -0,0 +1,34 @@
+package chapter3;
+
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner14;
+
+public class TestResults {
+    public static void main(String[] args) {
+        /*
+         * Test grade calculator
+         */
+
+        // Get Test Score
+        System.out.println("What is your test score? :");
+        Scanner scanner = new Scanner(System.in);
+        double testScore = scanner.nextDouble();
+
+        // Determine Grade
+        char grade;
+
+        if (testScore < 60)
+            grade = 'F';
+        else if (testScore < 70)
+            grade = 'D';
+        else if (testScore < 80)
+            grade = 'C';
+        else if (testScore < 90)
+            grade = 'B';
+        else
+            grade = 'A';
+
+        System.out.println("Your Grade is: " + grade);
+    }
+}
